03428a3638caaf7c530d61a30187128411cb1282
tanexc <artur.231456@gmail.com>
2023-07-27 12:54:47 +0500

    message repository edit

--- src/jvmMain/kotlin/ru/tanexc/application/data/repository/MessageRepositoryImpl.kt
+++ src/jvmMain/kotlin/ru/tanexc/application/data/repository/MessageRepositoryImpl.kt
@@ -8,27 +8,32 @@ import org.koin.core.component.inject
 import ru.tanexc.application.domain.interfaces.MessageDao
 import ru.tanexc.application.domain.repository.MessageRepository
 import util.State
+import util.exceptions.DataIsNull
 
 class MessageRepositoryImpl: MessageRepository, KoinComponent {
     private val messageDao: MessageDao by inject()
 
     override suspend fun create(data: Message): Flow<State<Message>> = flow {
-        emit(State.Processing())
-        val message: Message? = messageDao.insert(data)
-        if (message == null) {
-            emit(State.Error())
-        } else {
+        try {
+            emit(State.Processing())
+
+            val message: Message = messageDao.insert(data)?: throw DataIsNull()
+
             emit(State.Success(message))
+        } catch (e: Exception) {
+            emit(State.Error(message = e.message?: "creating message problem"))
         }
+
     }
 
     override suspend fun getById(id: Long): Flow<State<Message?>> = flow {
-        emit(State.Processing())
-        val message: Message? = messageDao.getById(id)
-        if (message == null) {
-            emit(State.Error())
-        } else {
+        try {
+            emit(State.Processing())
+            val message: Message = messageDao.getById(id)?: throw DataIsNull()
             emit(State.Success(message))
+        } catch (e: Exception) {
+            emit(State.Error(message = e.message?: "getting message by id = $id problem"))
         }
+
     }
 }
