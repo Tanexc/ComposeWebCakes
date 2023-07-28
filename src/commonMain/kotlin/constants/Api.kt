c879191ec5eb95a4d71220faca173b1e57d1add3
tanexc <artur.231456@gmail.com>
2023-07-28 19:06:23 +0500

    Api and websockets routes constants edit

--- src/commonMain/kotlin/constants/Api.kt
+++ src/commonMain/kotlin/constants/Api.kt
@@ -1,9 +1,21 @@
 package constants
 
 object Api {
-    const val GET_MESSAGES = "api/chat/get/all-message"
-    const val GET_MESSAGE = "api/chat/get/message"
+    // Message api
+    const val GET_MESSAGES = "api/message/get/chat" // get messages from chat with client id
+    const val GET_MESSAGE = "api/message/get" // get one message by id
+    const val POST_MESSAGE = "api/message/post" // post message
 
-    const val SET_MESSAGE = "api/chat/set/message"
+    // Chat api
+    const val GET_CHAT = "api/chat/get" // get chat by client id if exist
+    const val GET_NEW_CHAT = "api/chat/get/new" // get new chat with client id of not exist
+    const val POST_INTO_CHAT = "api/chat/post/message" // post message into chat
+
+    // User api
+    const val GET_USER = "api/user/get" // get chat by id or token
+    const val SIGN_IN_USER = "api/user/get/sign-in" // sign in user if exist
+    const val SIGN_UP_USER = "api/user/get/sign-up" // sign up user
+    const val UPDATE_CHATS_USER = "api/user/update/chat" // add chat id to user's chats
+    const val UPDATE_PASSWORD = "api/user/update/password" // updates password to newPassword
 
 }
---
+++ src/commonMain/kotlin/constants/Websocket.kt
@@ -0,0 +1,5 @@
+package constants
+
+object Websocket {
+    const val CHAT_WEBSOCKET = "websocket/chat"
+}
