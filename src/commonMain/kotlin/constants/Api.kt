df8fa1872c5b9554b5e5a5fbad442dbb67dda8f9
tanexc <artur.231456@gmail.com>
2023-07-30 00:37:10 +0500

    Api routes changes

--- src/commonMain/kotlin/constants/Api.kt
+++ src/commonMain/kotlin/constants/Api.kt
@@ -8,13 +8,13 @@ object Api {
     // Chat api
     const val GET_CHAT = "api/chat/get" // get chat by client id if exist
     const val GET_NEW_CHAT = "api/chat/get/new" // get new chat with client id of not exist
-    const val POST_INTO_CHAT = "api/chat/post/message" // post message into chat
+    const val POST_MESSAGE_INTO_CHAT = "api/chat/post/message" // post message into chat
 
     // User api
     const val GET_USER = "api/user/get" // get chat by id or token
     const val SIGN_IN_USER = "api/user/get/sign-in" // sign in user if exist
     const val SIGN_UP_USER = "api/user/get/sign-up" // sign up user
-    const val UPDATE_CHATS_USER = "api/user/update/chat" // add chat id to user's chats
-    const val UPDATE_PASSWORD = "api/user/update/password" // updates password to newPassword
+    const val UPDATE_CHATS_USER = "api/user/post/chat" // add chat id to user's chats
+    const val UPDATE_PASSWORD = "api/user/post/password" // updates password to newPassword
 
 }
