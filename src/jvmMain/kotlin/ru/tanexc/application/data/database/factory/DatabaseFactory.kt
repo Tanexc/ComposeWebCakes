45a3b00c5506a670d8eb1d7d779cefef02014c9f
tanexc <artur.231456@gmail.com>
2023-08-03 14:28:45 +0500

    database factory edit

--- src/jvmMain/kotlin/ru/tanexc/application/data/database/factory/DatabaseFactory.kt
+++ src/jvmMain/kotlin/ru/tanexc/application/data/database/factory/DatabaseFactory.kt
@@ -1,20 +1,26 @@
 package ru.tanexc.application.data.database.factory
 
-import io.ktor.server.config.*
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.koin.core.component.KoinComponent
+import ru.tanexc.application.core.util.config.ConfigTool
 import ru.tanexc.application.data.database.entity.ChatTable
 import ru.tanexc.application.data.database.entity.MessageTable
 import ru.tanexc.application.data.database.entity.UserTable
 
-object DatabaseFactory {
-    fun init(config: ApplicationConfig) {
+object DatabaseFactory: KoinComponent {
 
-        val driverClassName = config.property("storage.driverClassName").toString()
-        val jdbcURL = config.property("storage.jdbcURL").toString()
+    private val config = ConfigTool()
+
+    fun init() {
+
+        val driverClassName = config.getProperty("storage.driverClassName").toString()
+        val jdbcURL = config.getProperty("storage.jdbcURL").toString()
+
+        println(jdbcURL)
 
         val database = Database.connect(jdbcURL, driverClassName)
 
