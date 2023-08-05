9c478c23bc26ec2a2a4fd6e1dbc42ac457cac8b1
tanexc <artur.231456@gmail.com>
2023-08-05 22:15:49 +0500

    Client message api created

---
+++ src/jsMain/kotlin/data/api/message/MessageApiImpl.kt
@@ -0,0 +1,34 @@
+package data.api.message
+
+import constants.Api
+import constants.Application
+import domain.interfaces.MessageApi
+import domain.model.Message
+import domain.model.RespondData
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import org.koin.core.component.inject
+
+class MessageApiImpl: MessageApi {
+    private val client: HttpClient by inject()
+
+    override suspend fun getById(id: Long): RespondData<Message> =
+        client.get(urlString = "http://${Application.HOST}${Api.GET_MESSAGE}") {
+            url.parameters.appendAll(
+                parametersOf(
+                    "id" to listOf(id.toString())
+                )
+            )
+        }.body()
+
+    override suspend fun getByClientId(clientId: String): RespondData<List<Message>> =
+        client.get(urlString = "http://${Application.HOST}${Api.GET_MESSAGES}") {
+            url.parameters.appendAll(
+                parametersOf(
+                    "clientId" to listOf(clientId)
+                )
+            )
+        }.body()
+}
--- src/jsMain/kotlin/domain/interfaces/MessageApi.kt
+++ src/jsMain/kotlin/domain/interfaces/MessageApi.kt
@@ -1,4 +1,14 @@
 package domain.interfaces
 
-interface MessageApi {
+import domain.model.Message
+import domain.model.RespondData
+import kotlinx.coroutines.flow.Flow
+import org.koin.core.component.KoinComponent
+import util.State
+
+interface MessageApi: KoinComponent {
+
+    suspend fun getById(id: Long): RespondData<Message>
+
+    suspend fun getByClientId(clientId: String): RespondData<List<Message>>
 }
