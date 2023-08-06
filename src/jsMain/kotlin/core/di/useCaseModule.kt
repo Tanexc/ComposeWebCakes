b7c8191a9f328b449078b3cdb0d6a0ab17365263
tanexc <artur.231456@gmail.com>
2023-08-06 21:23:49 +0500

    di edit

--- src/jsMain/kotlin/core/di/useCaseModule.kt
+++ src/jsMain/kotlin/core/di/useCaseModule.kt
@@ -6,17 +6,10 @@ import data.repository.UserRepositoryImpl
 import domain.repository.ChatRepository
 import domain.repository.MessageRepository
 import domain.repository.UserRepository
-import domain.use_case.client.GetClientIdUseCase
-import domain.use_case.client.GetClientNameUseCase
-import domain.use_case.client.SetClientNameUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single<UserRepository> { UserRepositoryImpl() }
     single<ChatRepository> { ChatRepositoryImpl() }
     single<MessageRepository> { MessageRepositoryImpl() }
-
-    single { GetClientNameUseCase() }
-    single { GetClientIdUseCase() }
-    single { SetClientNameUseCase() }
 }
