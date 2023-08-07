f13dcc380e2c9f202a4cba8e667987fda4afedc1
tanexc <artur.231456@gmail.com>
2023-08-07 16:48:32 +0500

    util changes

--- src/commonMain/kotlin/constants/Application.kt
+++ src/commonMain/kotlin/constants/Application.kt
@@ -1,7 +1,8 @@
 package constants
 
 object Application {
-    const val HOST = "127.0.0.1"
+    const val HOST = "127.0.0.1:8080"
+    const val PORT = "8080"
 
     const val BASE_UI_URL = "/"
     const val BASE_UI_HTML_FILE = "index.html"
--- src/jsMain/kotlin/presentation/style/ui/theme/Typography.kt
+++ src/jsMain/kotlin/presentation/style/ui/theme/Typography.kt
@@ -1,8 +1,11 @@
 package presentation.style.ui.theme
 
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -28,6 +31,14 @@ val Typography = Typography(
         fontSize = 11.sp,
         lineHeight = 11.5.sp,
         letterSpacing = 0.5.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 9.sp,
+        lineHeight = 9.5.sp,
+        letterSpacing = 0.4.sp,
+        fontStyle = FontStyle.Italic
     )
 
 )
